376e1a2d5c0879fa2089b8cc01a04f4c9071a641
Nico Scheuring <nico.scheuring@gmail.com>
2022-05-21 03:42:26 +0200

    Fixed TicketStatus type

--- src/types/ticket_types.ts
+++ src/types/ticket_types.ts
@@ -1,14 +1,14 @@
 export enum TicketPriority {
-    LOW = 'low',
-    MEDIUM = 'medium',
-    HIGH = 'high',
-    CRITICAL = 'critical',
+    LOW = 'LOW',
+    MEDIUM = 'MEDIUM',
+    HIGH = 'HIGH',
+    CRITICAL = 'CRITICAL',
 }
 
 export enum TicketStatus {
-    WAITING_FOR_CUSTOMER = 'waitingForCustomer',
-    WAITING_FOR_SUPPORT = 'waitingForSupport',
-    IN_PROGRESS = 'inProgress',
-    CLOSED = 'closed',
+    WAITING_FOR_CUSTOMER = 'WAITING_FOR_CUSTOMER',
+    WAITING_FOR_SUPPORT = 'WAITING_FOR_SUPPORT',
+    IN_PROGRESS = 'IN_PROGRESS',
+    CLOSED = 'CLOSED',
 }
 

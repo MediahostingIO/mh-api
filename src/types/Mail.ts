57a15470b748ffdf1ca57fb5efa4f80557c89513
splinti <nico.scheuring@gmail.com>
2022-06-02 09:58:46 +0200

    MailOptions parameter

--- src/types/Mail.ts
+++ src/types/Mail.ts
@@ -1,28 +1,29 @@
 export interface Mail {
-	id: number;
-	subject: string;
-	contentHTML: string;
-	contentTEXT: string;
-	tag: MailTags;
-	created: Date;
-	userId: string;
+    id: number;
+    subject: string;
+    contentHTML: string;
+    contentTEXT: string;
+    tag: MailTags;
+    created: Date;
+    userId: string;
 }
 
 export interface MailResponse {
-	data: Mail[],
-	count: number,
+    data: Mail[],
+    count: number,
 }
 
 export interface MailOptions {
-	page?: number,
-	take?: number,
-	keyword?: string
+    page?: number,
+    take?: number,
+    keyword?: string,
+    tag: MailTags
 }
 
 export enum MailTags {
-	NONE = 'NONE',
-	NEWSLETTER = 'NEWSLETTER',
-	INVOICE = 'INVOICE',
-	ORDER = 'ORDER',
-	PRODUCT = 'PRODUCT',
+    NONE = 'NONE',
+    NEWSLETTER = 'NEWSLETTER',
+    INVOICE = 'INVOICE',
+    ORDER = 'ORDER',
+    PRODUCT = 'PRODUCT',
 }

a1a595431ea636c8caf7e2f8eb10b7cd0fc43df3
Florian Richter <info@florianrichter.eu>
2022-08-02 23:08:22 +0200

    updates

--- src/controller/team/UserNotes.ts
+++ src/controller/team/UserNotes.ts
@@ -2,11 +2,11 @@ import { SmallBaseUser } from "../../types/User";
 import { MHApi } from "../../mh-api";
 
 export default class UserNotes {
-	public async get(id: number): Promise<SmallBaseUser> {
+	public async get(id: number): Promise<{ userId: number, notes: string }> {
 		return MHApi.request('GET', 'team/customer/' + id);
 	}
 
-	public async update(id: number, notes: string): Promise<SmallBaseUser> {
+	public async update(id: number, notes: string): Promise<any> {
 		return MHApi.request('GET', 'team/customer/' + id, {data: { notes }});
 	}
 }

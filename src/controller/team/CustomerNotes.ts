45476bdaafd6007fb5d95135a79bfa0ecc40fff2
Florian Richter <info@florianrichter.eu>
2022-08-03 10:22:15 +0200

    fix customer note routes

--- src/controller/team/CustomerNotes.ts
+++ src/controller/team/CustomerNotes.ts
@@ -2,10 +2,10 @@ import { MHApi } from "../../mh-api";
 
 export default class CustomerNotes {
 	public async get(id: number): Promise<{ userId: number, notes: string }> {
-		return MHApi.request('GET', 'team/customer/' + id);
+		return MHApi.request('GET', 'team/notes/' + id);
 	}
 
 	public async update(id: number, notes: string): Promise<any> {
-		return MHApi.request('GET', 'team/customer/' + id, {data: { notes }});
+		return MHApi.request('GET', 'team/notes/' + id, {data: { notes }});
 	}
 }
